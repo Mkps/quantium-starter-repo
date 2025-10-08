842ad1777c61748045c03a87498e6f3c92c42ed0
Alexis <aloubier@student.42.com>
2025-10-08 18:59:10 +0200

    Bonus Task Six

---
+++ run_tests.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+set -e
+set -o pipefail
+
+source venv/bin/activate
+
+pytest -v
+EXIT_CODE=$?
+
+deactivate
+
+if [ $EXIT_CODE -eq 0 ]; then
+  echo "All tests successful"
+  exit 0
+else
+  echo "Tests failed"
+  exit 1
+fi
